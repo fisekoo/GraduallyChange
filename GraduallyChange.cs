cff7d8d82362e276887d5dbedd3756a04473dd6a
fisekoo <82342866+fisekoo@users.noreply.github.com>
2022-12-30 01:00:51 +0300

    Update GraduallyChange.cs

--- GraduallyChange.cs
+++ GraduallyChange.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 public static class GraduallyChange
 {
     /// <summary>Gradually changes a given value to target value.</summary>
@@ -6,28 +5,35 @@ public static class GraduallyChange
     /// <param name="callback">Lambda for assigning gradually changing value to a variable.</param>
     /// <param name="to">Target value.</param>
     /// <param name="duration">How long it will take start value to reach target value? In seconds.</param>
-    /// <param name="smoothness">How rough it will interpolate between start value and target value.</param>
-    public static System.Collections.IEnumerator To(System.Func<float> from, System.Action<float> callback, float to, float duration, float smoothness = 1)
+    /// <param name="isSmooth">Should interpolation be smooth?</param>
+    /// <param name="onComplete">Function when interpolation completed.</param>
+    public static System.Collections.IEnumerator To(System.Func<float> from, System.Action<float> callback, float to, float duration, bool isSmooth = false, System.Action onComplete = null)
     {
         var t = 0f;
         var current = from();
+        var a = from();
         while (current != to)
         {
-            t += Time.fixedDeltaTime;
-            t = Mathf.Clamp01(t / duration);
-            current = Lerp(current, to, t * smoothness);
+            t += UnityEngine.Time.fixedDeltaTime;
+            t = System.Math.Clamp(t, 0, duration);
+            current = isSmooth ? Lerp(current, to, (t / (duration * 10))) : Lerp(a, to, (t / duration));
             if (current.IsSimiliarTo(to))
             {
                 callback(to);
+                onComplete?.Invoke();
                 break;
             }
             callback(current);
-            yield return new WaitForFixedUpdate();
+            yield return new UnityEngine.WaitForFixedUpdate();
         }
     }
     /// <summary>Checks if a and b similiar to eachother by delta.</summary>
     /// <param name="delta">Minimum difference between numbers.</param>
     /// <returns>True if numbers are similiar</returns>
-    public static bool IsSimiliarTo(this float a, float b, float delta = 0.01f) => Mathf.Abs(b - a) <= delta;
+    public static bool IsSimiliarTo(this float a, float b, float delta = 0.01f) => System.Math.Abs(b - a) <= delta;
+
+    /// <summary>Linearly interpolate between a and b.</summary>
+    /// <param name="t">Interpolation value between values.</param>
+    /// <returns>Interpolated value between values</returns>
     public static float Lerp(float a, float b, float t) => (1 - t) * a + b * t;
 }
